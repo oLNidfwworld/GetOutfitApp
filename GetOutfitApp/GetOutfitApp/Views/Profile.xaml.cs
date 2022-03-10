8b802392d87df276b7688babb9b918f5012d86d0
Куветов Даниил <kuvetov@bk.ru>
2022-03-10 08:57:11 +0300

    ыфвывы

--- GetOutfitApp/GetOutfitApp/Views/Profile.xaml.cs
+++ GetOutfitApp/GetOutfitApp/Views/Profile.xaml.cs
@@ -15,6 +15,18 @@ namespace GetOutfitApp.Views
         public Profile()
         {
             InitializeComponent();
+            brand1.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand2.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand3.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand4.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand5.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand6.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand7.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand8.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand9.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand10.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand11.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
+            brand12.Source = ImageSource.FromResource("GetOutfitApp.Resources.chanel.jpg");
         }
     }
 }
